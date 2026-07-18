2180ec1e76bc9e5a5ae05ffb79e1de86b020b365
DevloperSP <teamrvabalaji@gmail.com>
2026-07-18 18:06:06 +0530

    update by devlopersp

--- MusicSp/utils/inline/start.py
+++ MusicSp/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text="𝓓𝓮𝓿𝓵𝓸𝓹𝓮𝓻 𝓗𝓸𝓶𝓮 ", url=f"https://t.me/Mecobots"),
+            InlineKeyboardButton(text="˹ 𝖱𝖾𝗉𝗈𝗌𝗂𝗍𝗈ʀʏ ˼", url="https://github.com/DevloperSP/MusicSp"),
         ],
     ]
     return buttons
